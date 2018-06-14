82d2d5de81ac04849d1cad38a27f7af1cba4bc77
Fabio Zadrozny <fabiofz@gmail.com>
2018-06-14 15:32:57 -0300

    Properly add files to manifest for sdist bdist_wheel.

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 
 """The setup script."""
 
-import os
-
 from setuptools import find_packages, setup
 
 with open('README.rst') as readme_file:
@@ -16,25 +14,12 @@ with open('HISTORY.rst') as history_file:
 requirements = ['Click>=6.0', ]
 
 setup_requirements = [
-#     'pytest-runner',
+    'pytest-runner',
 ]
 
 test_requirements = [
-#     'pytest',
+    'pytest',
 ]
-
-
-base = os.path.dirname(os.path.join(__file__))
-
-
-data_files = [
-    os.path.join(base, 'pydevf/pydev_formatter.jar'),
-    os.path.join(base, 'pydevf/pydev_formatter_lib/org.eclipse.equinox.common_3.10.0.v20180412-1130.jar'),
-    os.path.join(base, 'pydevf/pydev_formatter_lib/org.eclipse.text_3.6.300.v20180430-1330.jar'),
-]
-
-for f in data_files:
-    assert os.path.exists(f), 'Expected: %s to exist.' % (f,)
 
 
 setup(
@@ -69,7 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fabioz/pydevf',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
-    data_files=data_files,
 )
